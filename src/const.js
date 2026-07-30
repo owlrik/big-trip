08bb037b02ca82f0d90673b3ded5f9bc4de05817
Vadim Tiuriumin <owlriks@gmail.com>
2026-07-30 16:12:20 +0800

    Добавит презентер списка точек

---
+++ src/const.js
@@ -0,0 +1,3 @@
+const TRIP_EVENTS_COUNT = 3;
+
+export { TRIP_EVENTS_COUNT };
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
+import TripInfoView from './view/trip-info-view.js';
 import FilterView from './view/filter-view.js';
-import SortView from './view/sort-view.js';
+import TripEventsPresenter from './presenter/trip-events-presenter.js';
 
-import { render } from './render.js';
+import { render, RenderPosition } from './render.js';
 
+const tripMainElement = document.querySelector('.trip-main');
 const filtersContainerElement = document.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
 
+const tripEventsPresenter = new TripEventsPresenter({tripEventsContainer: tripEventsElement});
+
+render(new TripInfoView(), tripMainElement, RenderPosition.AFTERBEGIN);
 render(new FilterView(), filtersContainerElement);
-render(new SortView(), tripEventsElement);
+
+tripEventsPresenter.init();
